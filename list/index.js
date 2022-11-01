fc0db4669776a29f263dc1403728ed43cc1f6aaf
Jansix <117138890+Jansix@users.noreply.github.com>
2022-11-01 12:08:46 +0800

    v1

---
+++ list/index.js
@@ -0,0 +1,21 @@
+
+document.write(123);
+let myName = "夢修123" 
+console.log(myName)
+
+
+let key=500
+switch (key) {
+    case 100:
+        console.log('考了一百分');
+        break;
+    case 200:
+        console.log('考了兩百分');
+        break;
+    case 300:
+        console.log('考了三百分');
+        break;
+    default:
+        console.log('沒去考');
+        break;
+}
